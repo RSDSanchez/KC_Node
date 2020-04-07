0c318a28878f0212194fad3d4bd0af6ae1b819eb
geuv <geuvadan@gmail.com>
2020-04-07 09:43:28 +0200

    creado connectMongoose y Ad model

---
+++ models/Ad.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const adSchema = mongoose.Schema({
+  name: String,
+  type: String,
+  price: Number,
+  picture: String,
+  tags: [String],
+});
+
+adSchema.statics.lista = function (filtro, limit, skip, sort, fields) {
+  const query = Ad.find(filtro);
+  query.limit(limit);
+  query.skip(skip);
+  query.sort(sort);
+  query.select(fields);
+  return query.exec();
+};
+
+const Ad = mongoose.model('Ad', adSchema);
+
+module.exports = Ad;
